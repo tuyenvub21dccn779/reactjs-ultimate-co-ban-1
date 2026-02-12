d536fa71f85ae47f83ada87b6bdc3aa9a3c4adc1
Vu Hong Tuyen <tuyenvu524@gmail.com>
2026-02-12 21:51:08 +0700

    video 31. Cách sử dụng biến số với JSX

--- src/components/learn/MyComponent.jsx
+++ src/components/learn/MyComponent.jsx
@@ -6,9 +6,22 @@
 import './style.css';
 
 const MyComponent = () => {
+  // const hoidanit = "eric 1"; //string
+  // const hoidanit = 25; // number
+  // const hoidanit = true; //boolean
+  // const hoidanit = undefined;
+  // const hoidanit = null;
+  const hoidanit = [1, 2, 3];
+  // const hoidanit = {
+  //   name: "hoidanit",
+  //   age: 25
+  // }
+
+
   return (
     <>
-        <div> eric && hoidanit</div>
+        <div> {JSON.stringify(hoidanit)} && hoidanit</div>
+        <div>{console.log("ERIC")}</div>
         <div className="child"
           style={{borderRadius: "10px"}}
         >child</div>
