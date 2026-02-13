72114187462dcff4845586914aff9725eb8546c4
Vu Hong Tuyen <tuyenvu524@gmail.com>
2026-02-13 15:51:12 +0700

    video 33. Bài tập Components

---
+++ src/components/todo/TodoData.jsx
@@ -0,0 +1,10 @@
+const TodoData = () => {
+    return (
+        <div className='todo-data'>
+            <div>Learning React</div>
+            <div>Watching Youte</div>
+        </div>
+    )
+}
+
+export default TodoData;
---
+++ src/components/todo/TodoNew.jsx
@@ -0,0 +1,11 @@
+
+const TodoNew = () => {
+    return (
+        <div className='todo-new'>
+            <input type="text" placeholder="Enter your task" />
+            <button>Add</button>
+        </div>
+    )
+}
+
+export default TodoNew;
