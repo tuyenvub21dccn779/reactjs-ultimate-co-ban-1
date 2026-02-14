7cd32382b77a81cedeeb9ddc2228a87ddd5d299b
Vu Hong Tuyen <tuyenvu524@gmail.com>
2026-02-14 17:41:51 +0700

    video 40. Each child in a list should have a unique “key” prop

--- src/App.jsx
+++ src/App.jsx
@@ -7,16 +7,10 @@ import { useState } from 'react';
 const App = () => {
 
   const [todoList, setTodoList] = useState([
-    {id: 1, name: "Learning React"},
-    {id: 2, name: "Watching Youtube"}
+    // {id: 1, name: "Learning React"},
+    // {id: 2, name: "Watching Youtube"}
   ])
   
-  const hoidanit = "Tuyen Liverpool";
-  const age = 25;
-  const data = {
-    address: "hanoi",
-    country: "vietnam"
-  }
 
   const addNewTodo = (name) => {
     const newTodo = {
@@ -41,9 +35,6 @@ const App = () => {
         addNewTodo={addNewTodo}
       />
       <TodoData 
-        name={hoidanit}
-        age={age}
-        data={data}
         todoList={todoList}
       />
       <div className='todo-image'>
--- src/components/todo/TodoData.jsx
+++ src/components/todo/TodoData.jsx
@@ -7,7 +7,7 @@ const TodoData = (props) => {
         <div className='todo-data'>
             {todoList.map((item, index) => {
                 return (
-                    <div className="todo-item">
+                    <div className="todo-item" key={item.id}>
                         <div>{item.name}</div>
                         <button>Delete</button>
                     </div>
