7f48d7796350edbc0f431627764aa00ae529f8ba
Vu Hong Tuyen <tuyenvu524@gmail.com>
2026-02-14 19:32:39 +0700

    video 47. Tạo HeaderFooter

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import TodoData from './components/todo/TodoData';
 import TodoNew from './components/todo/TodoNew';
 import reactLogo from './assets/react.svg';
 import { useState } from 'react';
+import Header from './components/layout/header';
+import Footer from './components/layout/footer';
 
 const App = () => {
 
@@ -33,32 +35,37 @@ const App = () => {
 
 
   return (
-    <div className="todo-container">
-      <div className="todo-title">Todo List</div>
-      <TodoNew 
-        addNewTodo={addNewTodo}
-      />
-      {todoList.length > 0 ?
-        <TodoData 
-          todoList={todoList}
-          deleteTodo={deleteTodo}
+    <>
+      <Header />
+      <div className="todo-container">
+        <div className="todo-title">Todo List</div>
+        <TodoNew 
+          addNewTodo={addNewTodo}
         />
-        :
-        <div className='todo-image'>
-          <img src={reactLogo} className='logo'/>
-        </div>
-      }
-      {/*todoList.length > 0 &&
-        <TodoData 
-          todoList={todoList}
-        />
-      }
-      {todoList.length === 0 &&
-        <div className='todo-image'>
-          <img src={reactLogo} className='logo'/>
-        </div>
-      */}
-    </div>
+        {todoList.length > 0 ?
+          <TodoData 
+            todoList={todoList}
+            deleteTodo={deleteTodo}
+          />
+          :
+          <div className='todo-image'>
+            <img src={reactLogo} className='logo'/>
+          </div>
+        }
+        {/*todoList.length > 0 &&
+          <TodoData 
+            todoList={todoList}
+          />
+        }
+        {todoList.length === 0 &&
+          <div className='todo-image'>
+            <img src={reactLogo} className='logo'/>
+          </div>
+        */}
+
+      </div>
+      <Footer />
+    </>
   )
 }
 
---
+++ src/components/layout/footer.jsx
@@ -0,0 +1,12 @@
+import './footer.css';
+
+
+const Footer = () => {
+    return (
+        <div className="footer">
+            <p>React Vite - @TuyenB21DCCN779</p>
+        </div>
+    )
+}
+
+export default Footer;
