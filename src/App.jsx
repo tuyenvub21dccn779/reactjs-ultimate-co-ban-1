f3fa05c18b4ecf80a89ec50ad5984f4696c6d4b9
Vu Hong Tuyen <tuyenvu524@gmail.com>
2026-02-14 17:51:02 +0700

    video 41. Render với điều kiện

--- src/App.jsx
+++ src/App.jsx
@@ -34,12 +34,25 @@ const App = () => {
       <TodoNew 
         addNewTodo={addNewTodo}
       />
-      <TodoData 
-        todoList={todoList}
-      />
-      <div className='todo-image'>
-        <img src={reactLogo} className='logo'/>
-      </div>
+      {todoList.length > 0 ?
+        <TodoData 
+          todoList={todoList}
+        />
+        :
+        <div className='todo-image'>
+          <img src={reactLogo} className='logo'/>
+        </div>
+      }
+      {/*todoList.length > 0 &&
+        <TodoData 
+          todoList={todoList}
+        />
+      }
+      {todoList.length === 0 &&
+        <div className='todo-image'>
+          <img src={reactLogo} className='logo'/>
+        </div>
+      */}
     </div>
   )
 }
