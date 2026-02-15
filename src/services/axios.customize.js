92c7801c8df6607a0c7a2c1f92f959c053e1cb49
Vu Hong Tuyen <tuyenvu524@gmail.com>
2026-02-15 13:01:59 +0700

    video 70. Config Axios Interceptor

---
+++ src/services/axios.customize.js
@@ -0,0 +1,38 @@
+import axios from "axios";
+
+// Set config defaults when creating the instance
+const instance = axios.create({
+  baseURL: "http://localhost:8080",
+});
+
+// Alter defaults after instance has been created
+// instance.defaults.headers.common["Authorization"] = AUTH_TOKEN;
+
+// Add a request interceptor
+instance.interceptors.request.use(
+  function (config) {
+    // Do something before the request is sent
+    return config;
+  },
+  function (error) {
+    // Do something with the request error
+    return Promise.reject(error);
+  },
+);
+
+// Add a response interceptor
+instance.interceptors.response.use(
+  function (response) {
+    // Any status code that lies within the range of 2xx causes this function to trigger
+    // Do something with response data
+    if(response.data && response.data.data) return response.data;
+    return response;
+  },
+  function (error) {
+    // Any status codes that fall outside the range of 2xx cause this function to trigger
+    // Do something with response error
+    return Promise.reject(error);
+  },
+);
+
+export default instance;
