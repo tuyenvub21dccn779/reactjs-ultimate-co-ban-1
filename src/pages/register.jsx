9cf6c7284fe595c8cc5118b03ee67acbef7bedc4
Vu Hong Tuyen <tuyenvu524@gmail.com>
2026-02-17 17:59:58 +0700

    Video 91. Chia Layout Responsive (Bonus)

--- src/pages/register.jsx
+++ src/pages/register.jsx
@@ -1,4 +1,4 @@
-import { Button, Input, Form, notification, message } from "antd";
+import { Button, Input, Form, notification, Row, Col } from "antd";
 import { registerUserAPI } from "../services/api.service";
 import { useNavigate } from "react-router-dom";
 
@@ -11,11 +11,11 @@ const RegisterPage = () => {
 
         //call api 
         const res = await registerUserAPI(
-            values.fullName, 
-            values.email, 
-            values.password, 
+            values.fullName,
+            values.email,
+            values.password,
             values.phone);
-        if(res.data) {
+        if (res.data) {
             notification.success({
                 message: "Register user",
                 description: "Đăng ký user thành công"
@@ -34,56 +34,71 @@ const RegisterPage = () => {
             form={form}
             layout="vertical"
             onFinish={onFinish}
+            style={{ margin: "10px" }}
         // onFinish={onFinish}
         // onFinishFailed={onFinishFailed}
         >
-
-            <div style={{
-                margin: "50px",
-            }}>
-                <Form.Item
-                    label="Full Name"
-                    name="fullName"
-                    rules={[{ required: true, message: 'Please input your fullName!' }]}
-                >
-                    <Input />
-                </Form.Item>
-                <Form.Item
-                    label="Email"
-                    name="email"
-                    rules={[{ required: true, message: 'Please input your email!' }]}
-                >
-                    <Input />
-                </Form.Item>
-                <Form.Item
-                    label="Password"
-                    name="password"
-                    rules={[{ required: true, message: 'Please input your password!' }]}
-                >
-                    <Input.Password />
-                </Form.Item>
-                <Form.Item
-                    label="Phone Number"
-                    name="phone"
-                    rules={[{
-                        required: true,
-                        pattern: new RegExp(/\d+/g),
-                        message: "Wrong format!"
-                    }]}
-                >
-                    <Input />
-                </Form.Item>
-
-                <div>
-
-                    <Button
-                        onClick={() => form.submit()}
-                        type="primary"
+            <Row justify={"center"}>
+                <Col xs={24} md={8}>
+                    <Form.Item
+                        label="Full Name"
+                        name="fullName"
+                        rules={[{ required: true, message: 'Please input your fullName!' }]}
                     >
-                        Register
-                    </Button>
-                </div>
-            </div>
+                        <Input />
+                    </Form.Item>
+                </Col>
+            </Row>
+            <Row justify={"center"}>
+                <Col xs={24} md={8}>
+                    <Form.Item
+                        label="Email"
+                        name="email"
+                        rules={[{ required: true, message: 'Please input your email!' }]}
+                    >
+                        <Input />
+                    </Form.Item>
+                </Col>
+            </Row>
+            <Row justify={"center"}>
+                <Col xs={24} md={8}>
+                    <Form.Item
+                        label="Password"
+                        name="password"
+                        rules={[{ required: true, message: 'Please input your password!' }]}
+                    >
+                        <Input.Password />
+                    </Form.Item>
+                </Col>
+            </Row>
+            <Row justify={"center"}>
+                <Col xs={24} md={8}>
+                    <Form.Item
+                        label="Phone Number"
+                        name="phone"
+                        rules={[{
+                            required: true,
+                            pattern: new RegExp(/\d+/g),
+                            message: "Wrong format!"
+                        }]}
+                    >
+                        <Input />
+                    </Form.Item>
+                </Col>
+            </Row>
+            <Row justify={"center"}>
+                <Col xs={24} md={8}>
+                    <div>
+
+                        <Button
+                            onClick={() => form.submit()}
+                            type="primary"
+                        >
+                            Register
+                        </Button>
+                    </div>
+                </Col>
+            </Row>
         </Form>
     )
 }
