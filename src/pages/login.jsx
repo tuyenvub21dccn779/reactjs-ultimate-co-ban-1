b5c5366d21426650234e841a12b95b5d3dac2d5f
Vu Hong Tuyen <tuyenvu524@gmail.com>
2026-02-17 20:47:01 +0700

    video 92. Làm Bài Tập Design Login

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,7 +1,94 @@
+import { Col, Row, Form, Input, Button, Divider } from "antd";
+import { Link } from "react-router-dom";
 
 const LoginPage = () => {
+    const [form] = Form.useForm();
+
+    const onFinish = async (values) => {
+        console.log(">>> check values: ", values);
+
+        //call api 
+        // const res = await registerUserAPI(
+        //     values.fullName,
+        //     values.email,
+        //     values.password,
+        //     values.phone);
+        // if (res.data) {
+        //     notification.success({
+        //         message: "Register user",
+        //         description: "Đăng ký user thành công"
+        //     });
+        //     navigate("/login");
+        // } else {
+        //     notification.error({
+        //         message: "Register user error",
+        //         description: JSON.stringify(res.message)
+        //     });
+        // }
+    }
+
+
     return (
-        <div>login page</div>
+
+        <Row justify="center">
+            <Col xs={24} md={12} style={{
+                border: "1px solid #ddd",
+                padding: "16px",
+                borderRadius: "4px",
+                marginTop: "50px",
+            }}>
+                <p style={{ 
+                    position: "relative",
+                    display: "inline-block",
+                    background: "white",
+                    top: "-25px",
+                    fontSize: "medium"
+                 }}>Đăng nhập</p>
+                <Form
+                    form={form}
+                    layout="vertical"
+                    onFinish={onFinish}
+                // style={{ margin: "10px" }}
+                // onFinish={onFinish}
+                // onFinishFailed={onFinishFailed}
+                >
+                    <Form.Item
+                        label="Email"
+                        name="email"
+                        rules={[{ required: true, message: 'Please input your email!' }]}
+                    >
+                        <Input />
+                    </Form.Item>
+
+                    <Form.Item
+                        label="Password"
+                        name="password"
+                        rules={[{ required: true, message: 'Please input your password!' }]}
+                    >
+                        <Input.Password />
+                    </Form.Item>
+                    <Row justify="space-between" align="middle">
+                        <Col>
+                            <Button
+                                onClick={() => form.submit()}
+                                type="primary"
+                            >
+                                Login
+                            </Button>
+                        </Col>
+                        <Col>
+                            <Link to={"/"}>Go to homepage</Link>
+                        </Col>
+                    </Row>
+                    <Divider />
+                    <div style={{ 
+                        textAlign: "center"
+                     }}>
+                        Chưa có tài khoản? <Link to={"/register"}>Đăng ký tại đây</Link>
+                    </div>
+                </Form>
+            </Col>
+        </Row>
     )
 }
 
