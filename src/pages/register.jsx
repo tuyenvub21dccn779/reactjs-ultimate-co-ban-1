6dcb6600116ffc398a1fdabd17c29e1bd1f84be2
Vu Hong Tuyen <tuyenvu524@gmail.com>
2026-02-17 17:21:27 +0700

    video 89. Sử Dụng Uncontrolled Component Cho Register

--- src/pages/register.jsx
+++ src/pages/register.jsx
@@ -1,7 +1,64 @@
+import { Button, Input, Form } from "antd";
 
 const RegisterPage = () => {
+    const [form] = Form.useForm();
+
+    const onFinish = (values) => {
+        console.log(">>> check values: ", values);
+    }
+
     return (
-        <div>register page</div>
+        <Form
+            form={form}
+            layout="vertical"
+            onFinish={onFinish}
+        // onFinish={onFinish}
+        // onFinishFailed={onFinishFailed}
+        >
+
+            <div style={{
+                margin: "50px",
+            }}>
+                <Form.Item
+                    label="Full Name"
+                    name="fullName"
+                    // rules={[{ required: true, message: 'Please input your username!' }]}
+                >
+                    <Input />
+                </Form.Item>
+                <Form.Item
+                    label="Email"
+                    name="email"
+                    // rules={[{ required: true, message: 'Please input your username!' }]}
+                >
+                    <Input />
+                </Form.Item>
+                <Form.Item
+                    label="Password"
+                    name="password"
+                    // rules={[{ required: true, message: 'Please input your username!' }]}
+                >
+                    <Input.Password />
+                </Form.Item>
+                <Form.Item
+                    label="Phone Number"
+                    name="phone"
+                    // rules={[{ required: true, message: 'Please input your username!' }]}
+                >
+                    <Input />
+                </Form.Item>
+                
+                <div>
+                
+                    <Button
+                        onClick={() => form.submit()}
+                        type="primary"
+                    >
+                        Register
+                    </Button>
+                </div>
+            </div>
+        </Form>
     )
 }
 
