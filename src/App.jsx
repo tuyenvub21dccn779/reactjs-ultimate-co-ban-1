f16f610518b84c704d3314a1a3a31be49581f835
Vu Hong Tuyen <tuyenvu524@gmail.com>
2026-02-18 13:32:40 +0700

    video 101. Private Route với React

--- src/App.jsx
+++ src/App.jsx
@@ -5,38 +5,44 @@ import { Outlet } from 'react-router-dom';
 import { getAccountAPI } from './services/api.service';
 import { useContext, useEffect } from 'react';
 import { AuthContext } from './components/context/auth.context';
+import { Spin } from 'antd';
 
 
 
 const App = () => {
 
-  const {setUser} = useContext(AuthContext);
+  const { setUser, isAppLoading, setIsAppLoading } = useContext(AuthContext);
 
   useEffect(() => {
     fetchUserInfo();
   }, []);
 
-  const delay = (miliSeconds) => {
-    return new Promise((resolve, reject) => {
-      setTimeout(() => {
-        resolve();
-      }, miliSeconds);
-    });
-  };
-
   const fetchUserInfo = async () => {
     const res = await getAccountAPI();
-    await delay(3000);
-    if(res.data) {
+    if (res.data) {
       setUser(res.data.user);
     }
+    setIsAppLoading(false);
   }
 
   return (
     <>
-      <Header />
-      <Outlet />
-      <Footer />
+      {isAppLoading === true ?
+        <div style={{ 
+          position: "fixed",
+          top: "50%",
+          left: "50%",
+          transform: "translate(-50%, -50%)",
+         }}>
+          <Spin />
+        </div>
+        :
+        <>
+          <Header />
+          <Outlet />
+          <Footer />
+        </>
+      }
     </>
   )
 }
